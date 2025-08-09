3ff55235404af9e03e544f79f4a0e2c30cbe310d
qr-koishi <1946214535@qq.com>
2025-08-09 18:54:35 +0800

    input class

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -4,6 +4,7 @@
 #include "Hazel/Log.h"
 
 #include "glad/glad.h"
+#include "Input.h"
 
 namespace Hazel
 {
@@ -17,7 +18,7 @@ namespace Hazel
 
 	Application::Application()
 	{
-		HZ_CORE_ASSERT(s_Instance, "Application already exists!");
+		HZ_CORE_ASSERT(!s_Instance, "Application already exists!");
 		s_Instance = this;
 		m_Window = std::unique_ptr<Window>(Window::Create());
 		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
@@ -68,6 +69,9 @@ namespace Hazel
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+			auto [x, y] = Input::GetMousePosition();
+
+			HZ_CORE_TRACE("{0},{1}",x, y);
 
 			m_Window->OnUpdate();
 		}
