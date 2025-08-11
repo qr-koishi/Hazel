c92940948310c6111acd6fd07b295c98111b8b60
qr-koishi <1946214535@qq.com>
2025-08-11 15:39:00 +0800

    mouse and key codes

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -7,8 +7,9 @@
 #include "Hazel/Layer.h"
 #include "Hazel/Log.h"
 #include "Hazel/ImGui/ImGuiLayer.h"
-
-
+#include "Hazel//Input.h"
+#include "Hazel/MouseButtonCodes.h"
+#include "Hazel/KeyCodes.h"
 
 //Entry Point
 #include "Hazel/EntryPoint.h"
