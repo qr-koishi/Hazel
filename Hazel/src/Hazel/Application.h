4c2b7c915e6b8dc0082f563b876ea5903e124791
qr-koishi <1946214535@qq.com>
2025-08-17 16:37:32 +0800

    Added VertexArray

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -10,6 +10,7 @@
 
 #include "Hazel/Redener/Shader.h"
 #include "Hazel/Redener/Buffer.h"
+#include "Hazel/Redener/VertexArray.h"
 
 namespace Hazel
 {
@@ -39,10 +40,11 @@ namespace Hazel
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray;
-		std::unique_ptr<Shader>m_Shader;
-		std::unique_ptr<VertexBuffer> m_VertexBuffer;
-		std::unique_ptr<IndexBuffer> m_IndexBuffer;
+		std::shared_ptr<VertexArray> m_VertexArray;
+		std::shared_ptr<Shader>m_Shader;
+
+		std::shared_ptr<VertexArray> m_SquareVA;
+		std::shared_ptr<Shader>m_BlueShader;
 	private:
 		static Application* Application::s_Instance;
 	};
